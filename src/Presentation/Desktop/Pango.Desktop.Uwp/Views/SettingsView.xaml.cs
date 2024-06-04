68fc642c81e6c90aa49f8f993578810f2c1f457f
Dzmitry Shauchuk <dshauchuk@gmail.com>
2024-06-04 21:55:57 +0300

    feat(settings): add an "about" section into settings

--- src/Presentation/Desktop/Pango.Desktop.Uwp/Views/SettingsView.xaml.cs
+++ src/Presentation/Desktop/Pango.Desktop.Uwp/Views/SettingsView.xaml.cs
@@ -3,6 +3,8 @@ using Pango.Desktop.Uwp.Core.Attributes;
 using Pango.Desktop.Uwp.Core.Enums;
 using Pango.Desktop.Uwp.ViewModels;
 using Pango.Desktop.Uwp.Views.Abstract;
+using System;
+using Windows.System;
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
@@ -15,5 +17,10 @@ public sealed partial class SettingsView : PageBase
     {
         this.InitializeComponent();
         DataContext = Ioc.Default.GetRequiredService<SettingsViewModel>();
+    }
+
+    private async void bugRequestCard_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+    {
+        await Launcher.LaunchUriAsync(new Uri("https://github.com/Dshauchuk/pango/issues/new/choose"));
     }
 }
