bb47b98aba8593cbf2fa6dad49efcee894aff07e
Dzmitry Shauchuk <dshauchuk@gmail.com>
2024-08-18 22:22:45 +0300

    feat(import&export): add dedicated views for import & export
    refs: #38

--- src/Presentation/Desktop/Pango.Desktop.Uwp/ViewModels/ExportImportViewModel.cs
+++ src/Presentation/Desktop/Pango.Desktop.Uwp/ViewModels/ExportImportViewModel.cs
@@ -1,14 +1,68 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Mvvm.Input;
+using Microsoft.Extensions.Logging;
 using Pango.Desktop.Uwp.Core.Attributes;
 using Pango.Desktop.Uwp.Core.Enums;
+using System;
 
 namespace Pango.Desktop.Uwp.ViewModels;
 
 [AppView(AppView.ExportImport)]
 public sealed class ExportImportViewModel : ViewModelBase
 {
+    #region Fields
+
+    private int _selectedOption;
+
+    #endregion
+
     public ExportImportViewModel(ILogger<ExportImportViewModel> logger) : base(logger)
     {
-
+        ExportDataCommand = new RelayCommand(OnExportDataAsync);
+        ImportDataCommand = new RelayCommand(OnImportDataAsync);
+        NavigateToOptionCommand = new RelayCommand<int>(OnNavigateToOption);
     }
+
+
+    #region Commands
+
+    public RelayCommand ExportDataCommand { get; }
+    public RelayCommand ImportDataCommand { get; }
+    public RelayCommand<int> NavigateToOptionCommand { get; }
+
+    #endregion
+
+    #region Properties
+
+
+    public int SelectedOption
+    {
+        get => _selectedOption;
+        set => SetProperty(ref _selectedOption, value);
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    private async void OnImportDataAsync()
+    {
+    }
+
+    private async void OnExportDataAsync()
+    {
+    }
+
+    private void OnNavigateToOption(int option)
+    {
+        SelectedOption = option switch
+        {
+            0 => 0,
+            1 => 1,
+            2 => 2,
+            _ => throw new InvalidOperationException($"Option {option} cannot be navigated")
+        };
+    }
+
+    #endregion
+
 }
