0fa1064f67887e7687e920722f8ddcb872777be7
Dzmitry Shauchuk <dshauchuk@gmail.com>
2024-09-02 21:07:43 +0300

    feat(import&export): add ExportDataValidator
    refs: #38

--- src/Presentation/Desktop/Pango.Desktop.Uwp/Dialogs/ViewModels/ExportDialogViewModel.cs
+++ src/Presentation/Desktop/Pango.Desktop.Uwp/Dialogs/ViewModels/ExportDialogViewModel.cs
@@ -9,13 +9,32 @@ using Pango.Desktop.Uwp.Dialogs.Parameters;
 using Pango.Desktop.Uwp.ViewModels;
 using Pango.Persistence.File;
 using System;
+using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 
 namespace Pango.Desktop.Uwp.Dialogs.ViewModels;
 
 public class ExportDataValidator : ObservableValidator
 {
-    private string _owner
+    private string _owner = string.Empty;
+    private string _masterPassword = string.Empty;
+
+    [Required]
+    [MinLength(3)]
+    [MaxLength(20)]
+    public string Owner
+    {
+        get => _owner;
+        set => SetProperty(ref _owner, value);
+    }
+
+    [Required]
+    [MinLength(3)]
+    public string MasterPassword
+    {
+        get => _masterPassword;
+        set => SetProperty(ref _masterPassword, value);
+    }
 }
 
 public class ExportDialogViewModel : ViewModelBase, IDialogViewModel
@@ -24,35 +43,30 @@ public class ExportDialogViewModel : ViewModelBase, IDialogViewModel
 
     private readonly ISender _sender;
     private readonly IUserContextProvider _userContextProvider;
+    private readonly IPasswordHashProvider _passwordHashProvider;
     private ExportDataParameters? _parameters;
 
-    private string _owner;
-    private string _masterPassword;
-    private string _exportingItemsInfo;
+    private string _exportingItemsInfo = string.Empty;
+    private ExportDataValidator _validator;
 
     #endregion
 
-    public ExportDialogViewModel(ILogger<ExportDialogViewModel> logger, ISender sender, IUserContextProvider userContextProvider)
+    public ExportDialogViewModel(
+        ILogger<ExportDialogViewModel> logger, 
+        ISender sender, 
+        IUserContextProvider userContextProvider, 
+        IPasswordHashProvider passwordHashProvider)
         : base(logger)
     {
         DialogContext = new DialogContext();
+
         _sender = sender;
+        _validator = new();
         _userContextProvider = userContextProvider;
+        _passwordHashProvider = passwordHashProvider;
     }
 
     #region Properties
-
-    public string Owner
-    {
-        get => _owner;
-        set => SetProperty(ref _owner, value);
-    }
-
-    public string MasterPassword
-    {
-        get => _masterPassword;
-        set => SetProperty(ref _masterPassword, value);
-    }
 
     public string ExportingItemsInfo
     {
@@ -60,8 +74,13 @@ public class ExportDialogViewModel : ViewModelBase, IDialogViewModel
         set => SetProperty(ref _exportingItemsInfo, value);
     }
 
-    #endregion
+    public ExportDataValidator Validator
+    {
+        get => _validator;
+        set => SetProperty(ref _validator, value);
+    }
 
+    #endregion
 
     public IDialogContext DialogContext { get; }
 
@@ -77,14 +96,17 @@ public class ExportDialogViewModel : ViewModelBase, IDialogViewModel
 
     public async Task OnSaveAsync()
     {
-        var t = await _userContextProvider.GetEncodingOptionsAsync();
+        string passwordHash = _passwordHashProvider.Hash(Validator.MasterPassword, out var saltBytes);
+        string salt = Convert.ToBase64String(saltBytes);
 
-        var a = Convert.FromBase64String(t.Key);
-        var b = a[..16];
+        var encoding = new EncodingOptions(passwordHash, salt);
+
+        //var t = await _userContextProvider.GetEncodingOptionsAsync();
+        //var a = Convert.FromBase64String(t.Key);
+        //var b = a[..16];
+        //var encoding = new EncodingOptions(t.Key, Convert.ToBase64String(b));
 
         string tmpUser = "tmp";
-        var encoding = new EncodingOptions(t.Key, Convert.ToBase64String(b));
-
         ErrorOr<ExportResult> result =
             await _sender.Send(new ExportDataCommand(_parameters.Items, new ExportOptions(tmpUser, encoding)));
     }
@@ -93,7 +115,9 @@ public class ExportDialogViewModel : ViewModelBase, IDialogViewModel
     {
         await base.OnNavigatedToAsync(parameter);
 
-        if(parameter is not ExportDataParameters dialogParameters)
+        ResetDialog();
+
+        if (parameter is not ExportDataParameters dialogParameters)
         {
             return;
         }
@@ -103,4 +127,14 @@ public class ExportDialogViewModel : ViewModelBase, IDialogViewModel
 
         ExportingItemsInfo = $"{_parameters.Items.Count} passwords";
     }
+
+
+    #region Private Methods
+
+    private void ResetDialog()
+    {
+        Validator = new();
+    }
+
+    #endregion
 }
