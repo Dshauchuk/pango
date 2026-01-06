5614846b86003904044953218df9a83f28dd0045
Dzmitry Shauchuk <dshauchuk@gmail.com>
2026-01-06 23:46:02 +0300

    build: update nuget packages

--- src/Presentation/Desktop/Pango.Desktop.Uwp/Properties/AssemblyInfo.cs
+++ src/Presentation/Desktop/Pango.Desktop.Uwp/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("The Cats Lab")]
 [assembly: AssemblyProduct("Pango.Desktop.Uwp")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -23,6 +23,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.4.0")]
-[assembly: AssemblyFileVersion("0.1.4.0")]
+[assembly: AssemblyVersion("0.1.5.0")]
+[assembly: AssemblyFileVersion("0.1.5.0")]
 [assembly: ComVisible(false)]
