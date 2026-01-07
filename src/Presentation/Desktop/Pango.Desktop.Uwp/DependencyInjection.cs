0cef7fbb5172995d26ffc3a0324426a81b2b39eb
Dzmitry Shauchuk <dshauchuk@gmail.com>
2026-01-07 23:54:21 +0300

    feat: change the file capacity to 20

--- src/Presentation/Desktop/Pango.Desktop.Uwp/DependencyInjection.cs
+++ src/Presentation/Desktop/Pango.Desktop.Uwp/DependencyInjection.cs
@@ -80,7 +80,7 @@ public static class DependencyInjection
 
         // DS
         // TODO: move to the config file
-        services.AddSingleton<IAppOptions>((s) => new AppOptions(new FileOptions() { PasswordsPerFile = 2 }));
+        services.AddSingleton<IAppOptions>((s) => new AppOptions(new FileOptions() { PasswordsPerFile = 20 }));
 
         return services;
     }
