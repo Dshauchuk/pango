0579d656a8e971b213a8bae0e6517c08675e5b38
MrGitV <142344641+MrGitV@users.noreply.github.com>
2026-02-23 01:31:31 +0300

    feat: make the app launch at startup if the setting is selected

---
+++ src/Presentation/Desktop/Pango.Desktop.Uwp/Core/Utility/Contracts/IStartupService.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+namespace Pango.Desktop.Uwp.Core.Utility.Contracts;
+
+/// <summary>
+/// Interface for managing the application startup state
+/// </summary>
+public interface IStartupService
+{
+    Task<bool> IsStartupEnabledAsync();
+
+    Task<bool> EnableStartupAsync();
+
+    Task DisableStartupAsync();
+}
