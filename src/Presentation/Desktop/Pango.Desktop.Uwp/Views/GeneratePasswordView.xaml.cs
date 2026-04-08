978636a87fc0639478b046ed7e2d78f58a52efba
Catherine <katerina.loev@gmail.com>
2026-04-08 13:00:13 +0300

    refactor: improve password generator UI for better user experience

--- src/Presentation/Desktop/Pango.Desktop.Uwp/Views/GeneratePasswordView.xaml.cs
+++ src/Presentation/Desktop/Pango.Desktop.Uwp/Views/GeneratePasswordView.xaml.cs
@@ -1,9 +1,15 @@
 using CommunityToolkit.Mvvm.Messaging;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Media.Animation;
 using Pango.Desktop.Uwp.Core.Attributes;
 using Pango.Desktop.Uwp.ViewModels;
 using Pango.Desktop.Uwp.Views.Abstract;
+using System;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -21,5 +27,24 @@ public sealed partial class GeneratePasswordView : PageBase
     {
         InitializeComponent();
         DataContext = App.Host.Services.GetRequiredService<GeneratePasswordViewModel>();
+
+        LengthTextBox.KeyDown += LengthTextBox_KeyDown;
+    }
+
+    private void LengthTextBox_KeyDown(object sender, KeyRoutedEventArgs e)
+    {
+        if (e.Key == Windows.System.VirtualKey.Enter)
+        {
+            e.Handled = true;
+
+            var binding = LengthTextBox.GetBindingExpression(TextBox.TextProperty);
+            binding?.UpdateSource();
+
+            // launch generation
+            if (DataContext is GeneratePasswordViewModel vm && vm.GenerateCommand.CanExecute(null))
+            {
+                vm.GenerateCommand.Execute(null);
+            }
+        }
     }
 }
